77d1965448ad4aa2ca09e6fe31bca7a79fe8107b
xiaoXingX <2736752685@qq.com>
2017-02-15 22:53:38 +0800

    update confi
    confi upd

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-type:text/html;charset=utf-8");
-$pdo=new PDO("mysql:host=localhost;dbname=kkxddata","root","tzmysql0769",array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION)) or die('连接失败');
+$pdo=new PDO("mysql:host=localhost;dbname=kkxddata","root","123456",array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION)) or die('连接失败');
 	 
 	$pdo->query("set names utf8");
 ?>
