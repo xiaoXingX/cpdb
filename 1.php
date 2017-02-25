6b1b47f8ffc9dbd06c29391d35f62a8c38768828
xiaoXingX <2736752685@qq.com>
2017-02-25 12:34:17 +0800

    25日

---
+++ 1.php
@@ -0,0 +1,25 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Person {
+
+    function __construct() {
+        
+    }
+    function test($a) {
+        while ($row = mysql_fetch_array($query)) {
+            
+        }
+        /* @var $array type */
+        $a=  [1,23,3];
+        foreach ($a as $ky => $vaalue) {
+            
+        }
+    }
+
+}
